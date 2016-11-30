e7c0d46dcdb6df44589c84a27961b76aaf045dcf
Todd Caine <tcaine@sigsegv.home>
2016-11-29 19:30:00 -0800

    Adding constants for simplify the setting of IPv4 header DSCP field.
    Exposing session configuration via GetConfig().
    
    UDP connection decorator being used for setting IP TTL and DSCP/TOS values.

--- client/session.go
+++ client/session.go
@@ -7,6 +7,30 @@ import (
 	"net"
 )
 
+const (
+	BE   = 0x00
+	CS1  = 0x20
+	AF11 = 0x28
+	AF12 = 0x30
+	AF13 = 0x38
+	CS2  = 0x40
+	AF21 = 0x48
+	AF22 = 0x50
+	AF23 = 0x58
+	CS3  = 0x60
+	AF31 = 0x68
+	AF32 = 0x70
+	AF33 = 0x78
+	CS4  = 0x80
+	AF41 = 0x88
+	AF42 = 0x90
+	AF43 = 0x98
+	CS5  = 0xA0
+	EF   = 0xB8
+	CS6  = 0xC0
+	CS7  = 0xE0
+)
+
 type TwampSession struct {
 	conn   *TwampConnection
 	port   uint16
@@ -15,6 +39,10 @@ type TwampSession struct {
 
 func (s *TwampSession) GetConnection() net.Conn {
 	return s.conn.GetConnection()
+}
+
+func (s *TwampSession) GetConfig() TwampSessionConfig {
+	return s.config
 }
 
 func (s *TwampSession) Write(buf []byte) {
@@ -56,7 +84,7 @@ func (s *TwampSession) CreateTest() (*TwampTest, error) {
 	}
 
 	conn, err := net.DialUDP("udp", localAddr, remoteAddr)
-	test.conn = conn
+	test.SetConnection(conn)
 
 	if err != nil {
 		log.Printf("Some error %+v", err)
